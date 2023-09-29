fa2b7d8f110b25c5acc60b0c669c00d05e3ac272
MayankRai30 <102607416+MayankRai30@users.noreply.github.com>
2023-09-29 09:16:59 +0530

    misc.py

--- Zaid/modules/basic/misc.py
+++ Zaid/modules/basic/misc.py
@@ -17,7 +17,7 @@ if ALIVE_TEXT:
    txt = ALIVE_TEXT
 else:
     txt = (
-        f"** SIXTY NINE**\n\n"
+        f"** SIXTY NINE **\n\n"
         f"❏ **νєяѕισи**: `2.1`\n"
         f"├• **υρтιмє**: `{str(datetime.now() - START_TIME).split('.')[0]}`\n"
         f"├• **ρутнσи**: `{python_version()}`\n"
